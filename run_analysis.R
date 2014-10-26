ddb52241f71e53956e43c7ebe437df6e664992ad
Barbara Graham Andre <mba@greyrock.org>
2014-10-26 15:58:33 -0600

    First commit

---
+++ run_analysis.R
@@ -0,0 +1,33 @@
+################################################################################
+#
+# This program merges and analyzes training and test sets from the files 
+    available at
+#  https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+#  downloaded 10/25/2014
+#
+#  Download and unzipping code is not included. Code can be run as long as 
+#    the Samsung data is in your working directory in a sub-folder titled
+#    "data" and with the same names as the unzipped files
+#
+
+
+## Step 1: merge the training and test sets to create one data set
+
+
+## Step 2: extract only the measurements on the mean and standard deviation
+    for each measurement
+
+
+## Step 3: use descriptive activity names to name the activities in the data set
+
+
+## Step 4: Appropriately label the data set with descriptive variable names
+
+
+## Step 5: Create a second, independent tide data set with the average of each
+    variable for each activity and each subject
+
+## Submit: tidy data set; link to Github repository; code book describing
+  the variables, data, any transformations or work performed to clean
+  in CodeBook.md. Also include a README.md in the repo explains how all the
+    scripts work and how they are connected.
